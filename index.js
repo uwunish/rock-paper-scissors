6cd0738a9a1372c93bf3ef31f1e5e8a0922b177e
uwunish <96343499+uwunish@users.noreply.github.com>
2024-11-06 13:53:19 +0000

    Implement logic for winner for rock, paper or scissors

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Create a variable "computerChoice" and initialize it to the result of "getComputerChoice" function
-let computerChoice = getComputerChoice();
-
 // Create the "getComputerChoice" function
 function getComputerChoice() {
 	// Use the random function to generate the random number: 1, 2 or 3
@@ -20,9 +17,6 @@ function getComputerChoice() {
 	return choice;
 }
 
-// Create a variable "humanChoice" and intialize it to the result of "getHumanChoice" function
-let humanChoice = getHumanChoice();
-
 // Create a getHumanChoice function
 function getHumanChoice() {
 	// Ask the user "Rock, Paper or Scissors? " and return the choice to "humanChoice"
@@ -40,3 +34,54 @@ function getHumanChoice() {
 		getHumanChoice();
 	}
 }
+
+// Create variable "humanScore" and "computerScore" and initialize them to 0
+let humanScore = 0;
+let computerScore = 0;
+
+// Create the function "playRound" that takes "humanChoice" and "computerChoice" as arguments
+function playRound(humanChoice, computerChoice) {
+	if (humanChoice === computerChoice) {
+		console.log("It's a draw!");
+	} else if (humanChoice === "rock" && computerChoice === "paper") {
+		console.log("You lose! Paper beats Rock");
+		computerScore++;
+	} else if (humanChoice === "rock" && computerChoice === "scissors") {
+		console.log("You win! Rock beats Scissors");
+		humanScore++;
+	} else if (humanChoice === "paper" && computerChoice === "rock") {
+		console.log("You win! Paper beats Rock");
+		humanScore++;
+	} else if (humanChoice === "paper" && computerChoice === "scissors") {
+		console.log("You lose! Scissors beats Paper");
+		computerScore++;
+	} else if (humanChoice === "scissors" && computerChoice === "rock") {
+		console.log("You lose! Rock beats Scissors");
+		computerScore++;
+	} else if (humanChoice === "scissors" && computerChoice === "paper") {
+		console.log("You win! Scissors beats Paper");
+		humanScore++;
+	}
+}
+
+// Create the function "playGame" which calls "playRound" five times
+function playGame() {
+	computerScore = 0;
+	humanScore = 0;
+	for (let i = 0; i < 5; i++) {
+		// Create a variable "computerChoice" and initialize it to the result of "getComputerChoice" function
+		let computerChoice = getComputerChoice();
+
+		// Create a variable "humanChoice" and intialize it to the result of "getHumanChoice" function
+		let humanChoice = getHumanChoice();
+
+		playRound(humanChoice, computerChoice);
+	}
+	if (humanScore > computerScore) {
+		console.log("Congratulations! You are the ultimate winner...");
+	} else {
+		console.log("Alas! Computer was the ultimate winner...");
+	}
+}
+
+playGame();
